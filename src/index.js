8e03b2990cf3a99713c7a2e00a7766f95a7d4f7d
Mitchell Rose <mmrose@cogdata.com>
2020-12-12 10:07:43 -0500

    prep for publish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import "./styles.css";
 import { Tree } from "./tree";
-//import { Dump } from 'react-dump';
 
-//const tree = Tree.factory();
-
-document.getElementById("app").innerHTML = `
-<div id="header"/>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+export { Tree };
